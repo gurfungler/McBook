57f7f5efe07f1c54e2e455e3382ab6cf947164b3
mickey <mickey@Ubuntu.myguest.virtualbox.org>
2023-10-15 12:41:38 -0700

    made some files

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
 import { useState, useEffect } from "react";
+import Navbar from "./Components/Navbar";
 
 const RouteSwitch = () => {
   const [backendData, setBackendData] = useState([{}]);
@@ -11,11 +14,16 @@ const RouteSwitch = () => {
         setBackendData(data);
       });
   }, []);
-  return(
+  return (
     <div>
-        <BrowserRouter>
-        
-        </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 };
